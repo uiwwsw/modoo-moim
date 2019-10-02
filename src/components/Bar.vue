87c7ba030cb13855072f4776b1e7ce6aceedac89
uiwwnw@adwitt.com <Ah5ALWtx5XTHrn5>
2019-10-02 15:59:16 +0900

    init

---
+++ src/components/Bar.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app-bar
+    fixed
+    dense
+    dark
+  >
+    <v-toolbar-title>{{title}}</v-toolbar-title>
+
+    <div class="flex-grow-1"></div>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-menu
+      left
+      bottom
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on">
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="n in 5"
+          :key="n"
+          @click="() => {}"
+        >
+          <v-list-item-title>Option {{ n }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
+
+<style lang="scss" scoped>
+  .v-sheet {
+    flex: none;
+  }
+</style>
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: 'Bar',
+        props: ['title'],
+    });
+</script>
---
+++ src/components/Bottom.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-bottom-navigation
+    :value="routeIndex"
+    dark
+    shift
+    fixed
+  >
+    <v-btn v-for="router in $store.state.routers" @click="goPage(router.name)">
+      <span>{{router.name.charAt(0).toUpperCase() + router.name.slice(1)}}</span>
+      <v-icon>{{router.icon}}</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+<style lang="scss" scoped>
+  .v-bottom-navigation {
+    .v-btn {
+      height: 56px !important;
+    }
+  }
+</style>
+<script lang="ts">
+import Vue from 'vue';
+import _ from 'lodash';
+
+export default Vue.extend({
+    name: 'Bottom',
+    props: ['routeIndex'],
+    methods: {
+        goPage(path: string) {
+            if (this.routeIndex !== _.findIndex(this.$store.state.routers, {name: path})) {
+                this.$router.push({
+                    path,
+                });
+            }
+        },
+    },
+});
+</script>
